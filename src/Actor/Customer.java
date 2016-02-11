a109034e86b42a6655f9864e62ac5d33f0b26d55
sam-sfsu <scandito@mail.sfsu.edu>
2016-02-11 14:59:26 -0800

    Adds Javadocs to Actor classes

--- src/Actor/Customer.java
+++ src/Actor/Customer.java
@@ -11,16 +11,25 @@ import Model.SaledItem;
 import post.Sale;
 
 /**
- *
- * @author ninjung
+ * Actor class for a customer of a store. Customer can create invoice with saleBuilder() and make purchases with MakePurchase().
  */
 public class Customer {
     private String name;
     Sale sale;
+    
+    /**
+     * Constructor method. Creates customer with a name passed as a parameter in the function call.
+     * @param name Name of new customer.
+     */
     public Customer(String name){
         this.name = name;
     }
     
+    /**
+     * Builds an invoice for a customer. Products added are checked with the passed ProductCatalog object.
+     * @param catalog ProductCatalog object for a store. Includes UPCs and associated product descriptions and prices.
+     * @return Sale object built from products added from catalog by builder.
+     */
     public Sale saleBuilder(ProductCatalog catalog){
         sale = new Sale(getName()); // insert customer name
         
@@ -38,19 +47,27 @@ public class Customer {
         return sale;
     }
     
+    /**
+     * Makes purchase on a sale for a customer. 
+     * @param paymentType String containing payment type: "cash" for cash transaction; "card" for card transaction.
+     * @param paymentAmount The amount to be paid; generally the balance of the current sale.
+     * @param cardNo 16-digit number of credit card number. If paymentType is "cash", NULL can be passed.
+     */
     public void MakePurchase(String paymentType, double paymentAmount, String cardNo){
         sale.makePayment(paymentType,paymentAmount,sale.getAmount(),cardNo);
     }
 
     /**
-     * @return the name
+     * Getter method; retrieves name of a customer.
+     * @return String containing name of customer.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * @param name the name to set
+     * Setter method; sets name of a customer to name passed as parameter.
+     * @param name String containing new name of customer.
      */
     public void setName(String name) {
         this.name = name;
--- src/Actor/Manager.java
+++ src/Actor/Manager.java
@@ -10,24 +10,37 @@ import Model.ProductItem;
 import post.Store;
 
 /**
- *
- * @author ninjung
+ * Actor class for a manager of a store. Manager can open store with Openstore() and manage a product catalog with manageCatalog().
  */
 public class Manager {
     private String username;
     private String password;
     Store store = new Store();
+    
+    /**
+     * Constructor method. Creates manager with name and password passed as parameters in the function call.
+     * @param username Name of new manager.
+     * @param password Password of new manager.
+     */
     public Manager(String username, String password){
         this.username = username;
         this.password = password;
     }
     
+    /**
+     * Creates/opens a store for a manager.
+     * @return Boolean verifying login of manager. True on success; false on failure.
+     */
     public boolean Openstore(){
         store = new Store();
         boolean isLogin = store.verifyLogin(this.getUsername(), this.getPassword());
         return isLogin;
     }
     
+    /**
+     * Allows manager to add and retrieve the product catalog of a store.
+     * @return The product catalog of a store.
+     */
     public ProductCatalog manageCatalog(){
         store = new Store();
         ProductCatalog catalog = store.getProductCatalog();
@@ -50,14 +63,16 @@ public class Manager {
         return catalog;
     }
     /**
-     * @return the username
+     * Getter method; retrieves username of a manager.
+     * @return The username of a manager.
      */
     public String getUsername() {
         return username;
     }
 
     /**
-     * @return the password
+     * Getter method; retrieves password of a manager.
+     * @return The password of a manager.
      */
     public String getPassword() {
         return password;
